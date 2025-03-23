9a6d00942ae94bffc456b9aa74c593ca4b5e6487
4by3 <timothywastaken@hotmail.com>
2025-03-23 19:40:51 +1100

    Improved output clarity

--- dijkstrasAlgorithm.py
+++ dijkstrasAlgorithm.py
@@ -9,14 +9,21 @@ visited = set()
 
 while minHeap:
     weight, node = heapq.heappop(minHeap)
-    if any(node == destination for destination in destinations):
-        print("Got to destination " + str(node) + " in " + str(weight))
-        break
     if node in visited:
+        print(f"Node {node} already visited")
         continue
-    print("On node " + str(node))
+    print(f"\nCurrently:\nAt node {node + 1} with current distance {weight}")
+    if any(node == destination for destination in destinations):
+        print(f"\nFinished\nGot to destination {node + 1} in {weight}.")
+        break
     visited.add(node)
+    print("Adding node edges:")
     for edge in edges[node]:
         if edge[0] not in visited:
             heapq.heappush(minHeap, (edge[1] + weight, edge[0]))
-            print("Pushed " + str(edge[0]))
+            print(f"Pushed node {edge[0] + 1} with distance {edge[1] + weight}.")
+        else:
+            print(f"Node {edge[0] + 1} has already been visited.")
+    print("Priority Queue Size:")
+    for value in minHeap:
+        print(f"Node: {value[1]} Size: {value[0]}")
